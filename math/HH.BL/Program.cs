bf7e6928e43162f3e1b55515e620033ff1424891
Radistb <61047860+Radistb@users.noreply.github.com>
2020-09-30 15:20:31 +0300

    DX + procents + math lib lesson

---
+++ math/HH.BL/Program.cs
@@ -0,0 +1,15 @@
+﻿using HH.BL.question2;
+using HH.BL.question3;
+using System;
+
+namespace HH.BL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Group1Fdx group1Fdx = new Group1Fdx();
+            group1Fdx.Calculate();
+        }
+    }
+}
---
+++ math/HH.BL/question1/Group2DX.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HH.BL.question1
+{
+    class Group2DX
+    {
+        public void Calculate()
+        {
+            double xn, xk, dx, a, b;
+            double y = 0.0;
+            string s1, s2, s3, s4, s5;
+
+            Console.WriteLine("Введите xn = ");
+            s1 = Console.ReadLine();
+            xn = Convert.ToDouble(s1);
+
+            double x = xn;
+
+            Console.WriteLine("Введите xk = ");
+            s2 = Console.ReadLine();
+            xk = Convert.ToDouble(s2);
+
+            Console.WriteLine("Введите dx = ");
+            s3 = Console.ReadLine();
+            dx = Convert.ToDouble(s3);
+
+            Console.WriteLine("Введите a = ");
+            s4 = Console.ReadLine();
+            a = Convert.ToDouble(s4);
+
+            Console.WriteLine("Введите b = ");
+            s5 = Console.ReadLine();
+            b = Convert.ToDouble(s5);
+
+            Console.WriteLine($"Input Data: \n" +
+                $"xn = { s1},\n" +
+                $"xK = { s2},\n" +
+                $"dx = { s3},\n" +
+                $"a = { s4},\n" +
+                $"b = { s5}");
+
+            while (xk >= x)
+            {
+                y = (System.Math.Sin(x - a))
+                                /
+                    (System.Math.Exp(a - x)
+                                +
+                    (System.Math.Sqrt(System.Math.Abs(b * x))));
+
+                Console.WriteLine("If x = " + x + ", y = " + y);
+
+                x = x + dx;
+            }
+        }
+    }
+}
