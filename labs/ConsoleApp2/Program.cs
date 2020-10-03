fb97d8a691b75c64c0b315f69ad0354a14df6901
Radistb <61047860+Radistb@users.noreply.github.com>
2020-10-03 19:34:33 +0300

    adeed navigation lab

---
+++ labs/ConsoleApp2/Program.cs
@@ -0,0 +1,33 @@
+﻿using ConsoleApp2.View;
+using System;
+using System.Collections;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = 0;
+
+            ArrayList Menu1_Items = new ArrayList();
+            Menu1_Items.Add("Help");
+            Menu1_Items.Add("File");
+            Menu1_Items.Add("Edit");
+            Menu1_Items.Add("Exit");
+
+            
+
+            UserInterface Menu = new UserInterface(Menu1_Items, false, 10, 10);
+            index = Menu.Show(0);
+            Console.WriteLine(index);
+            Console.ReadLine();
+
+
+            /*
+             
+             
+             */
+        }
+    }
+}
---
+++ labs/ConsoleApp2/View/UserInterface.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2.View
+{
+    public class UserInterface
+    {
+        public ConsoleColor TextColor = ConsoleColor.White;
+        public ConsoleColor FonColor = ConsoleColor.DarkYellow;
+        public ConsoleColor MenuColor = ConsoleColor.DarkYellow;
+        public ConsoleColor ItemsColog = ConsoleColor.Blue;
+        public ArrayList Items = new ArrayList();
+        public int x;//початкова позиція меню
+        public int y;//початкова позиція меню
+        public int w;//ширина меню
+        public int h;//висота меню
+        public string menuStr;//порожні йрядок меню
+        public int index;//обраний елемент меню
+
+        public UserInterface(ArrayList s, bool menyType, int xPos, int yPos)
+        {
+
+            string tmp;
+            this.Items = s;
+            x = xPos;
+            y = yPos;
+            h = this.Items.Count + 1;
+            //визначаємо ширину меню
+            w = 16;
+            for (int i = 0; i < this.Items.Count; i++)
+            {
+                tmp = (string)this.Items[i];
+                if (tmp.Length + 2 > w) { w = tmp.Length + 2; }
+            }
+            //створення рядків меню
+            //довжина коротких рядків збільшуеться до ширини меню
+            for (int i = 0; i < Items.Count; i++)
+            {
+                tmp = " " + (string)Items[i];
+                if (tmp.Length < w - 2)
+                {
+                    for (int k = tmp.Length; k < w - 2; k++) { tmp = tmp + " "; }
+                }
+                Items[i] = tmp;
+
+            }
+            //вимикаємо курсор
+            Console.CursorVisible = false;
+        }
+
+        //вимикаємо курсор
+
+        public void DrawBackGroundMenu()
+        {
+            //малюємо основу меню
+            {
+                //створюєморядок текстудовжиною w
+                menuStr = "";
+                for (int i = 0; i < w; i++) { menuStr = menuStr + " "; }
+                //малюємоосновуменю
+                Console.BackgroundColor = MenuColor;
+                for (int i = 0; i <= h; i++)
+                {
+                    Console.SetCursorPosition(x, i + y);
+                    Console.Write(menuStr);
+                    //return 1;
+                }
+            }
+        }
+
+        public void DrawItemsMenu()
+        {
+            for (int i = 0; i < Items.Count; i++)
+            {
+                if (i == index) Console.BackgroundColor = ItemsColog;
+                else Console.BackgroundColor = FonColor;
+                Console.SetCursorPosition(x + 1, i + y + 1);
+                Console.Write(Items[i]);
+                Console.BackgroundColor = MenuColor;
+            }
+        }
+
+        //скануємо клавішіта переміщуємо курсор
+        public void ScanMenu()
+        {
+            ConsoleKeyInfo key;
+            do
+            {
+                DrawItemsMenu();
+                key = Console.ReadKey();
+                if (key.Key == ConsoleKey.DownArrow)
+                {
+                    index++;
+                    if (index > 3) index = 0;
+                }
+                if (key.Key == ConsoleKey.UpArrow)
+                {
+                    index--;
+                    if (index < 0) index = 3;
+                }
+            } while (key.Key != ConsoleKey.Enter);
+        }
+
+        public int Show(int Start)
+        {
+            index = Start; //малюємо основу меню
+            DrawBackGroundMenu(); //малюємо опції
+            DrawItemsMenu(); //скануємо клавішітапереміщуємо курсор
+            ScanMenu();
+            return index;
+        }
+    }
+}
+
+
